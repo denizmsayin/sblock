010b32c64dcc3fe52eaa37987e82b39d72bd5334
Deniz MERCADIER SAYIN <denizmsayin@gmail.com>
2019-11-28 23:32:50 +0300

    added masking comments

--- masked_sbpuzzle.hpp
+++ masked_sbpuzzle.hpp
@@ -7,8 +7,43 @@
 // where part of the puzzle remains indistinguishable
 // the class is exactly the same apart from the apply_action function,
 // since moving a tile which is not in the group is zero cost
+
+// one significant issue for the disjoint pattern database implementation
+// is that moves of the blank tile are not counted. This means that we would
+// want to return a cost of 0 for moves where a tile from the group is not
+// moved. Unfortunately, this renders BFS unoptimal due to not being a unit
+// cost step, which hinders the construction of the database. Since it would
+// be much more costly to run A* from each state to the puzzle solution,
+// the only answer I could come up with is greatly extending the amount of
+// actions possible so that each action contains all possible unit cost moves
+// from the current state, which may imply multiple moves of the empty tile. 
+// e.g. :
+// ------------- Four possible unit cost moves:
+// | 0 | 1 | X | ... 0 8 1 . 0 8 X . 0 1 X . 0 1 X
+// ------------- ... 3 X X . 3 1 X . 8 3 X . 3 X X
+// | 3 | X | 8 | ... 6 X X . 6 X X . 6 X X . 8 6 X
+// -------------
+// | 6 | X | X |
+// -------------
+// Another unfortunate consequence is that changing the signature of the possible
+// actions function requires breaking inheritance and replacing it with composition.
+// This is nice, but not very nice in the way that it requires forwarding of all functions
+
+// A final observation of mine is that the classic SBPuzzle is a specific instance
+// of the masked puzzle where all the tiles are in a single group. The only
+// missing part now is that we want to be able to request using different sets of
+// actions. To allow this, we simply need to make the possible_actions() member
+// function templated, so that it can return different action types depending
+// on the template argument type.
+
+struct MaskedAction {
+    uint8_t new_hole_pos, new_tile_pos;
+
+    MaskedAction(uint8_t a, uint8_t b) : new_hole_pos(a), new_tile_pos(b) {}
+};
+
 template <int H, int W>
-class MaskedSBPuzzle : public SBPuzzle<H, W> 
+class MaskedSBPuzzle 
 {
 public:
     static const uint8_t _X = 255; // don't care tiles
@@ -17,13 +52,18 @@ public:
     explicit MaskedSBPuzzle(const int tiles[], const bool mask[]);
     explicit MaskedSBPuzzle(const SBPuzzle<H, W> &p, const bool mask[]);
 
+    std::vector<MaskedAction> possible_actions() const;
+
     int apply_action(Direction move);
+    int apply_action(MaskedAction a);
+
     bool is_solved() const;
 
     template <int HH, int WW>
     friend std::ostream &operator<<(std::ostream &s, const MaskedSBPuzzle<HH, WW> &p);
 
 private:
+    SBPuzzle<H, W> puzzle;
     void overwrite_tiles(const bool mask[]);
 };
 
