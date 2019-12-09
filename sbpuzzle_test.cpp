cdb8f0c1d1d461a048fc34ffc8219d7387a0f471
Deniz MERCADIER SAYIN <denizmsayin@gmail.com>
2019-12-09 18:05:00 +0300

    added ifile outfile to exe

--- sbpuzzle_test.cpp
+++ sbpuzzle_test.cpp
@@ -63,8 +63,9 @@ void show_usage() {
     std::string sstring = make_string_list(ext);
     std::string hstring = make_string_list(sbpuzzle::HEURISTIC_STRINGS);
     std::cout 
-        << "Usage: ./exe_file [-h] [-r seed] [-n num-puzzles] [-s search-type] [-h heuristic]\n"
-        << "                  [-f file] [-w weight] [-b batch_size] [-g]\n"
+        << "Usage: ./exe_file [-?] [-r seed/source] [-n num-puzzles] [-s search-type]\n"
+        << "                  [-h heuristic] [-f file] [-w weight] [-b batch_size] [-g]\n"
+        << "                  [-i input_file] [-o output_file]\n"
         << "       -?             show this help message\n"
         << "       -r SEED        seed value used to generate puzzles, default is 42\n"
         << "       -n NUM_PUZZLES the number of puzzles to randomly generate, default is 1\n"
@@ -92,6 +93,12 @@ void show_usage() {
         << "       -b BATCH_SIZE  batch size for bwa*, important when using a dlmodel on gpu\n"
         << "                      default is 1\n"
         << "       -g             stands for GPU: if set, CUDA is enabled for dlmodels\n"
+        << "       -i INPUT_FILE  interpreted as a path to an input binary file containing\n"
+        << "                      puzzle-cost pairs (see generate_solutions.cpp). puzzles\n"
+        << "                      will be taken from this file rather than being randomly\n"
+        << "                      generated. Overrides -r and -n switches. \n"
+        << "       -o OUTPUT_FILE output file to save results in binary format as puzzle-cost\n" 
+        << "                      pairs (same format as source files). none by default.\n"
         ;
 }
 
@@ -102,6 +109,8 @@ int main(int argc, char *argv[]) {
     std::string search_type_str = "bfs";
     std::string heuristic_str = "manhattan";
     std::string file_path;
+    std::string in_path;
+    std::string out_path;
     double weight = 1.0;
     size_t batch_size = 1;
     bool use_gpu = false;
@@ -110,7 +119,7 @@ int main(int argc, char *argv[]) {
         std::cout << "For argument help: ./exe_file -?\n";
 
     int ch;
-    while((ch = getopt(argc, argv, "?gr:n:s:h:f:w:b:")) != -1) {
+    while((ch = getopt(argc, argv, "?gr:n:s:h:f:w:b:i:o:")) != -1) {
         switch(ch) {
             case '?':   show_usage(); return 0;
             case 'r':   seed = std::stoul(optarg); break;
@@ -121,16 +130,37 @@ int main(int argc, char *argv[]) {
             case 'w':   weight = std::stod(optarg); break;
             case 'b':   batch_size = std::stoull(optarg); break;
             case 'g':   use_gpu = true; break;
+            case 'i':   in_path = optarg; break;
+            case 'o':   out_path = optarg; break;
             default:    std::cerr << "Unexpected argument (" << static_cast<char>(ch) << "). " 
                                   << "Try ./exe_file -?\n"; return -1;
         }
     }
 
     // create n random puzzles
-    auto rng = std::default_random_engine(seed);
     std::vector<Puzzle> puzzles;
-    for(size_t i = 0; i < num_puzzles; i++) 
-        puzzles.push_back(create_solvable_puzzle<H, W>(rng));
+
+    if(in_path == "") { // generate puzzles
+        auto rng = std::default_random_engine(seed);
+        for(size_t i = 0; i < num_puzzles; i++) 
+            puzzles.emplace_back(create_solvable_puzzle<H, W>(rng));
+    } else { // read puzzles from file
+        // TODO: make this more elegant by using Puzzle::from_binary_stream
+        // currently it is not very practical because it is not suited to failure,
+        // i.e. reading the final puzzle (without using infile.peek())
+        num_puzzles = 0;
+        std::array<uint8_t, H*W> tiles;
+        char *tiles_ptr = reinterpret_cast<char *>(tiles.data());
+        size_t tiles_size = tiles.size() * sizeof(tiles[0]);
+        std::ifstream infile(in_path, std::ifstream::in | std::ifstream::binary);
+        while(infile.read(tiles_ptr, tiles_size)) { // while tiles can be read
+            puzzles.emplace_back(tiles); // insert the puzzle 
+            infile.ignore(1); // ignore the stored cost character
+            ++num_puzzles;
+        }
+        infile.close();
+        std::cout << "Read " << num_puzzles << " puzzles from file " << in_path << std::endl;
+    }
 
     using namespace sbpuzzle;
     using namespace search2;
@@ -148,7 +178,17 @@ int main(int argc, char *argv[]) {
     // in mind, it is necessary to use a reference to Heuristic rather than a pointer
     std::unique_ptr<Heuristic<H, W>> hp = heuristic_factory<H, W>(heuristic, file_path, use_gpu);
     Heuristic<H, W> &hf = *hp;
-    
+
+    // create a function to write the output
+    bool outfile_exists = (out_path != "");
+    std::ofstream outfile; 
+    if(outfile_exists) 
+        outfile = std::ofstream(out_path, std::ofstream::out | std::ofstream::binary);
+    auto writer = [&](const Puzzle &p, uint8_t cost) {
+        if(outfile_exists)
+            p.to_binary_stream(outfile) << cost;
+    };
+
 
     if(search_type_str != EVAL_HEURISTIC_STR) {
         SearchType search_type;
@@ -164,12 +204,13 @@ int main(int argc, char *argv[]) {
         auto t1 = std::chrono::high_resolution_clock::now();
         int num_moves = 0;
         size_t num_nodes = 0;
-        for(size_t i = 0; i < num_puzzles; i++) {
+        for(size_t i = 0, size = puzzles.size(); i < size; ++i) {
             using namespace search2;
             auto r = search_function(puzzles[i], weight, batch_size, std::ref(hf));
             num_moves += r.cost;
             num_nodes += r.nodes_expanded;
             std::cout << '\r' << (i+1) << "/" << num_puzzles << std::flush;
+            writer(puzzles[i], r.cost);
         }
  
         std::cout << '\r';
@@ -188,6 +229,9 @@ int main(int argc, char *argv[]) {
         std::vector<int> costs;
         hf(puzzles, costs);
         int total_cost = std::accumulate(costs.begin(), costs.end(), 0);
+        if(outfile_exists) // small check to prevent wasting time
+            for(size_t i = 0, size = puzzles.size(); i < size; ++i)
+                writer(puzzles[i], costs[i]);
         std::cout << "Evaluated " << num_puzzles << " puzzles, resulting in an average of "
                   << static_cast<double>(total_cost) / num_puzzles << " heuristic cost."
                   << std::endl;
