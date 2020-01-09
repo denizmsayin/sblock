9cadf5c1820659c257a1050bbea105a6d3b1a513
Deniz MERCADIER SAYIN <denizmsayin@gmail.com>
2020-01-09 04:24:43 +0300

    updated master header gen. to recurse

--- pysuite/generate_master_header.py
+++ pysuite/generate_master_header.py
@@ -4,6 +4,31 @@ from sys import argv
 
 import definitions as defs
 
+def generate_master_header(path, indent=0):
+
+    path = path.strip(os.sep)
+
+    path_defn = defs.to_header_guard(path)
+    defn_str = f'{defs.HEADER_GUARD_PROJECT_PREFIX}_{path_defn}_HPP'
+
+    base_path = os.path.basename(path)
+    p = os.path.join(os.path.dirname(path), f'{base_path}.hpp')
+
+    indent_str = '  ' * indent
+    print(indent_str + f'Generating {p}...')
+    with open(p, 'w') as header_file:
+        header_file.write(f'#ifndef {defn_str}\n')
+        header_file.write(f'#define {defn_str}\n\n')
+        for entry in os.scandir(path):
+            if entry.is_dir(): # recurse downwards
+                generate_master_header(entry.path, indent + 1)
+        for entry in os.scandir(path):
+            if entry.name.endswith('.hpp'):
+                header_file.write(f'#include "{base_path}/{entry.name}"\n')
+        header_file.write('\n#endif\n')
+    print(indent_str + f'Done generating {p}.')
+
+
 if __name__ == '__main__':
     
     if len(argv) != 2:
@@ -13,19 +38,5 @@ if __name__ == '__main__':
 
     _, folder = argv
 
-    folder = folder.strip(os.sep)
-
-    folder_defn = defs.to_header_guard(folder)
-    defn_str = f'{defs.HEADER_GUARD_PROJECT_PREFIX}_{folder_defn}_HPP'
-
-    base_folder = os.path.basename(folder)
-    p = os.path.join(os.path.dirname(folder), f'{base_folder}.hpp')
-    with open(p, 'w') as header_file:
-        header_file.write(f'#ifndef {defn_str}\n')
-        header_file.write(f'#define {defn_str}\n\n')
-        for entry in os.scandir(folder):
-            if entry.name.endswith('.hpp'):
-                header_file.write(f'#include "{base_folder}/{entry.name}"\n')
-        header_file.write('\n#endif\n')
-
+    generate_master_header(folder)
 
